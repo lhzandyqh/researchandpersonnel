af3f55c61937290dbfe4d6eb8cf168480231f7e3
lhzandyqh <yangqihang822@gmail.com>
2020-08-25 08:39:48 +0800

    20200824修改

---
+++ src/views/customInterface/projectManage/projectBegin.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app-container">
+    <h1>项目开题</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProjectBegin'
+}
+</script>
+
+<style scoped>
+
+</style>
