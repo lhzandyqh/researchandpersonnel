8467094d8ad6f0b92cb76a99f3728c65fd0a335d
lhzandyqh <yangqihang822@gmail.com>
2020-10-26 14:34:46 +0800

    20201026提交(完成后台对接，登陆接口对接前的准备提交)

--- src/views/customInterface/dataColletion/oneTimeCollection.vue
+++ src/views/customInterface/dataColletion/oneTimeCollection.vue
@@ -769,92 +769,16 @@
         </div>
       </el-dialog>
     </div>
-<!--    <el-card class="box-card">-->
-<!--      <div slot="header" class="clearfix">-->
-<!--        <span style="font-weight: bolder;color:#409EFF">基本信息</span>-->
-<!--        <el-button style="float: right; padding: 3px 0;margin-right: 20px" type="text">修改</el-button>-->
-<!--        <el-button style="float: right; padding: 3px 0;margin-right: 15px" type="text">查看</el-button>-->
-<!--      </div>-->
-<!--      <el-form ref="form" :model="form" label-width="150px">-->
-<!--        <el-row>-->
-<!--          <el-col :span="10">-->
-<!--            <el-form-item label="姓名">-->
-<!--              <el-input v-model="form.name" style="width: 300px" />-->
-<!--            </el-form-item>-->
-<!--          </el-col>-->
-<!--          <el-col :span="12"><el-form-item label="年龄">-->
-<!--            <el-input v-model="form.name" style="width: 310px;" />-->
-<!--          </el-form-item></el-col>-->
-<!--        </el-row>-->
-<!--        <el-row>-->
-<!--          <el-col :span="10"><el-form-item label="民族">-->
-<!--            <el-input v-model="form.name" style="width: 300px" />-->
-<!--          </el-form-item></el-col>-->
-<!--          <el-col :span="12">-->
-<!--            <el-form-item label="性别">-->
-<!--              <el-select v-model="form.region" style="width: 310px" placeholder="请选择性别">-->
-<!--                <el-option label="男" value="shanghai" />-->
-<!--                <el-option label="女" value="beijing" />-->
-<!--              </el-select>-->
-<!--            </el-form-item>-->
-<!--          </el-col>-->
-<!--        </el-row>-->
-<!--        <el-row>-->
-
-<!--          <el-col :span="10">-->
-<!--            <el-form-item label="身份证号">-->
-<!--              <el-input v-model="form.name" style="width: 300px;" />-->
-<!--            </el-form-item>-->
-<!--          </el-col>-->
-<!--          <el-col :span="12">-->
-<!--            <el-form-item label="入党时间">-->
-<!--              <el-date-picker v-model="form.date1" type="date" placeholder="选择日期" style="width: 310px;" />-->
-<!--            </el-form-item>-->
-<!--          </el-col>-->
-<!--        </el-row>-->
-<!--        <el-row>-->
-<!--          <el-col span="10">-->
-<!--            <el-form-item label="爱好">-->
-<!--              <el-cascader-->
-<!--                style="width: 300px"-->
-<!--                :options="options"-->
-<!--                :props="{ multiple: true }"-->
-<!--                clearable-->
-<!--              />-->
-<!--            </el-form-item>-->
-<!--          </el-col>-->
-<!--          <el-col span="12">-->
-<!--            <el-form-item label="上传附件">-->
-<!--              <el-upload-->
-<!--                ref="upload"-->
-<!--                class="upload-demo"-->
-<!--                action="https://jsonplaceholder.typicode.com/posts/"-->
-<!--                :on-preview="handlePreview"-->
-<!--                :on-remove="handleRemove"-->
-<!--                :file-list="fileList"-->
-<!--                :auto-upload="false"-->
-<!--              >-->
-<!--                <el-button slot="trigger" size="small" type="primary">选取文件</el-button>-->
-<!--                <el-button style="margin-left: 10px;background: #42b983" size="small" type="success" @click="submitUpload">上传</el-button>-->
-<!--              </el-upload>-->
-<!--            </el-form-item>-->
-<!--          </el-col>-->
-<!--        </el-row>-->
-<!--        <el-row>-->
-<!--          <div style="padding-top: 20px;text-align: center;">-->
-<!--            <el-button>保存</el-button>-->
-<!--            <el-button type="primary" @click="onSubmit">提交</el-button>-->
-<!--          </div>-->
-<!--        </el-row>-->
-<!--      </el-form>-->
-<!--    </el-card>-->
-
   </div>
 </template>
 
 <script>
+  import { teacherGetBasicInforma } from '@/api/teacherGetData'
 export default {
   name: 'OneTimeCollection',
+  mounted(){
+    this.getBasic()
+  },
   data() {
     return {
       sex:'',
@@ -938,6 +862,15 @@ export default {
     }
   },
   methods: {
+    getBasic: function(){
+      const prams = {
+        tid: 1
+      }
+      teacherGetBasicInforma(prams).then(response=>{
+        console.log('测试教师获取基本信息接口')
+        console.log(response.data)
+      })
+    },
     onSubmit() {
       console.log('submit!')
     },
